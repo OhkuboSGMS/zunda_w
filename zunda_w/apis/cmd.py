badfdffb63a2f2a74f92247d5112f05aad7feb5f
OkuboSGMS <ginger.os777146th@gmail.com>
2023-07-25 23:21:25 +0900

    sample_voiceコマンドを追加 (#48)
    特定のテキストをvoicevoxで実装されているキャラクタ一覧に特定のテキストを一括で読ませて出力する
    
    close #46 
    Co-authored-by: OhkuboSGMS <sinikake.gokiburi@gmail.com>

--- zunda_w/apis/cmd.py
+++ zunda_w/apis/cmd.py
@@ -1,8 +1,11 @@
 import shutil
+from pathlib import Path
 
 from loguru import logger
 from omegaconf import OmegaConf
+from pathvalidate import sanitize_filename
 
+from zunda_w import subtitle_util
 from zunda_w.voicevox import voice_vox, download_voicevox
 
 
@@ -31,9 +34,34 @@ def show_speaker(output_json: str, engine_dir: str):
     :return:
     """
     with voice_vox.voicevox_engine(download_voicevox.extract_engine(root_dir=engine_dir)):
-        # TODO context voicevox
         if speakers := voice_vox.get_speakers(output_json):
             print(voice_vox.format_speaker(speakers))
-            # logger.info(speakers)
+        else:
+            logger.warning('Can\'t get /speakers requests')
+
+
+def create_sample_voices(text: str, engine_dir: str, output: str = 'sample_voice'):
+    """voicevoxで実装されているキャラクターに同一を読み上げを行わせる
+    :param text:
+    :param engine_dir:
+    :param output:
+    :return:
+    """
+    output = Path(output)
+    output.mkdir(parents=True, exist_ok=True)
+    with voice_vox.voicevox_engine(download_voicevox.extract_engine(root_dir=engine_dir)):
+        if speakers := voice_vox.get_speakers():
+            output_names = []
+            request_list = []
+            for one_char in speakers:
+                name = one_char['name']
+                styles = one_char['styles']
+                for s in styles:
+                    file_name = f'{name}({s["name"]})'
+                    speaker_id = s['id']
+                    request_list.append(speaker_id)
+                    output_names.append(sanitize_filename(file_name))
+            subtitles = subtitle_util.from_proprietaries(text, request_list)
+            voice_vox.run(subtitles, root_dir=str(output), output_names=output_names, output_dir='', use_cache=False)
         else:
             logger.warning('Can\'t get /speakers requests')
