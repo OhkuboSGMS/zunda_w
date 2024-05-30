fb0bc0df98a15dce665de99b3152cc921662c717
OhkuboSGMS <ginger.os777146th@gmail.com>
2024-05-31 00:36:57 +0900

    update flet 0.12.1 -> 0.19.0

--- zunda_w/gui/main.py
+++ zunda_w/gui/main.py
@@ -1,8 +1,29 @@
 import asyncio
+import os
+from asyncio import sleep
+from pathlib import Path
 
 import flet as ft
-from flet_core import margin, FilePickerFileType
-from omegaconf import OmegaConf
+import yaml
+from flet_core import margin
+from loguru import logger
+from omegaconf import OmegaConf, SCMode
+from pydub import effects
+
+from zunda_w import file_hash
+from zunda_w.apis import share
+from zunda_w.srt_ops import sort_srt_files
+from zunda_w.util import file_uri, read_srt
+from zunda_w.words import WordFilter
+
+
+def read_preset(preset_name: str, preset_dir: str):
+    publish_conf_path = os.path.join(preset_dir, f"{preset_name}.yml")
+    if not os.path.exists(publish_conf_path):
+        logger.warning(f"Not Found Publish Preset: {publish_conf_path}")
+        return None
+
+    return yaml.load(open(publish_conf_path), Loader=yaml.SafeLoader)
 
 
 class AudioFile(ft.UserControl):
@@ -78,12 +99,18 @@ class AudioFile(ft.UserControl):
 
 class ConverterApp(ft.UserControl):
     is_convert = False
+    output_file = None
+    podcast_meta = {}
+    url = None
 
     def build(self):
+        # TODO 修正
+        PUBLISH_PRESET = list(map(lambda x: str(x.stem), Path(os.environ["APP_PUBLISH_PRESET_DIR"]).glob("*.yml")))
         from zunda_w.constants import update_preset
         update_preset("./preset_config")
         from zunda_w.constants import PRESET_NAME
 
+        self.hack_md_memo_text = ft.Text("HackMD Memo:<Empty>")
         self.audio_files = ft.Column(
             controls=[
                 AudioFile(self.task_status_change, self.delete),
@@ -95,15 +122,25 @@ class ConverterApp(ft.UserControl):
             options=[ft.dropdown.Option(key=p, text=p) for p in PRESET_NAME],
             value=PRESET_NAME[0]
         )
+        self.publish_select = ft.Dropdown(
+            width=150,
+            options=[ft.dropdown.Option(key=p, text=p) for p in PUBLISH_PRESET],
+            value=PUBLISH_PRESET[0]
+        )
+        self.note_select = ft.Dropdown(
+            width=150,
+        )
+
         self.progress = ft.Row(
             visible=False,
             controls=[
-                ft.Text("AAAA"),
+                ft.Text("Audio to Text to Audio..."),
                 ft.ProgressBar(width=400, color="amber", bgcolor="#eeeeee")
             ]
         )
         self.output_dir = ft.TextField(label="OutputDirectory")
-
+        self.publish_button = ft.ElevatedButton('Publish', visible=True, on_click=self.publish_to_podcast)
+        self.title_text = ft.Text("<title_text>")
         # application's root control (i.e. "view") containing all other controls
         return ft.Column(
             width=600,
@@ -112,6 +149,24 @@ class ConverterApp(ft.UserControl):
                 ft.Row(
                     [ft.Text(value="とにかくヨシ！Studio", style=ft.TextThemeStyle.HEADLINE_MEDIUM)],
                     alignment=ft.MainAxisAlignment.CENTER,
+                ),
+                ft.Row(
+                    controls=[
+                        ft.Text("Publish Preset:"),
+                        self.publish_select
+                    ],
+                    alignment=ft.MainAxisAlignment.SPACE_BETWEEN
+                ),
+                ft.Row(
+                    controls=[
+                        self.hack_md_memo_text,
+                        self.note_select,
+                        ft.ElevatedButton('Get Memo',
+                                          on_click=self.get_memos),
+                        ft.ElevatedButton('Construct Title',
+                                          on_click=self.get_memo_from_hackmd)
+                    ],
+                    alignment=ft.MainAxisAlignment.SPACE_BETWEEN
                 ),
                 ft.Divider(),
                 ft.Column(
@@ -149,34 +204,209 @@ class ConverterApp(ft.UserControl):
                         ft.ElevatedButton('Convert',
                                           on_click=self.convert),
                         self.progress
-
                     ],
                     alignment=ft.MainAxisAlignment.START
                 ),
+                ft.Row(
+                    controls=[
+                        ft.ElevatedButton('Construct', visible=True, on_click=self.publish_setting),
+                        self.title_text
+                    ],
+                    alignment=ft.MainAxisAlignment.START
+                ),
+                ft.Row(
+                    controls=[
+                        self.publish_button
+                    ],
+                    alignment=ft.MainAxisAlignment.START
+                )
             ],
         )
 
-    async def task_convert(self, files, preset):
+    async def get_memos(self, e):
+        """
+        HackMDのメモを取得して表示する
+        """
+        from zunda_w.apis.hackmd import get_notes
+        team_path = os.environ["HACKMD_TEAM_PATH"]
+        publish_conf = read_preset(self.publish_select.value, os.environ["APP_PUBLISH_PRESET_DIR"])
+        if publish_conf and publish_conf["note"]["tag"]:
+            notes = get_notes(team_path, tag=publish_conf["note"]["tag"])
+            self.note_select.options = [ft.dropdown.Option(key=n["id"], text=n["title"]) for n in notes]
+            self.note_select.value = notes[0]["id"]
+        await self.update_async()
+
+    async def get_memo_from_hackmd(self, e):
+        """
+        Construct Titleをクリックしたときにの動作
+        選択されたHackMdのメモを取得して
+        :param e:
+        :return:
+        """
+        from zunda_w.apis.hackmd import get_note
+        team_path = os.environ["HACKMD_TEAM_PATH"]
+        publish_conf = read_preset(self.publish_select.value, os.environ["APP_PUBLISH_PRESET_DIR"])
+        if not publish_conf or not publish_conf["note"]["tag"]:
+            raise ValueError("No tag in publish preset")
+
+        title, content = get_note(team_path, tag=publish_conf["note"]["tag"])
+        podcast_title = title
+        # TODO fix output style
+        output_dir = f"./output/{title}"
+        os.makedirs(output_dir, exist_ok=True)
+        # title_path = f"{output_dir}/title.txt"
+        # with open(title_path, "w") as f:
+        #     f.write(podcast_title)
+        memo_path = f"{output_dir}/memo.md"
+        with open(memo_path, "w") as f:
+            f.write(content)
+
+        self.podcast_meta = {
+            # "title": podcast_title,
+            "description": content,
+            # "title_path": title_path,
+            "memo_path": memo_path,
+        }
+        self.hack_md_memo_text.value = f"HackMD Memo:{title}"
+        self.output_dir.value = title
+        await self.update_async()
+
+    async def publish_setting(self, e):
+        from zunda_w.arg import Options
+        from zunda_w.apis import hackmd
+        if self.podcast_meta is None:
+            raise ValueError("No Podcast Meta")
+        if "memo_path" not in self.podcast_meta:
+            raise ValueError("Run Construct Title First!")
+        if "title" in self.podcast_meta:
+            return
+        if not self.output_dir.value:
+            raise ValueError("Run Construct Title First!")
+        memo_md = Path(self.podcast_meta["memo_path"]).read_text()
+        conf = Options()
+        conf.target_dir = self.output_dir.value
+        publish_index = int(Path(os.environ["APP_PUBLISH_PRESET_DIR"]).joinpath("n.txt").read_text()) + 1
+        with open(conf.tool_output("title.txt"), "w") as f:
+            title = hackmd.summarize_title(memo_md)
+            title = f"{publish_index}.{title}"
+            f.write(title)
+        print(conf.tool_output('title.txt'))
+        self.podcast_meta["title"] = title
+        self.podcast_meta["title_path"] = conf.tool_output("title.txt")
+
+        self.title_text.value = title
+        await self.update_async()
+
+    async def publish_to_podcast(self, e):
+        from zunda_w.apis.podcast_upload import publish
+        if self.output_file is None:
+            print("Not Found Publish File")
+            return
+        try:
+            # TODO shownoteのmarkdownをhtmlに変換
+            #     markdown.markdownFromFile(input=md, output=md.replace(".md", ".html"),
+            #                               extensions=["mdx_linkify"])
+            share_url = await publish(
+                os.environ["PODCAST_URL"],
+                os.environ["PODCAST_EMAIL"],
+                os.environ["PODCAST_PASSWORD"],
+                os.path.abspath(self.output_file),
+                Path(self.podcast_meta["title_path"]).read_text(),
+                Path(self.podcast_meta["memo_path"]).read_text(),
+                # TODO publish.ymlから取得,
+                timeout=360 * 1000  # 出すまでに時間がかかるので、長めに取っておく
+                # thumbnail=os.environ["PODCAST_THUMBNAIL"] if "PODCAST_THUMBNAIL" in os.environ else None,
+            )
+            self.url = share_url
+            # OGタグが生成されるまで待つ
+            await sleep(5)
+            await share(self.url, None)
+        except Exception as e:
+            logger.exception(e)
+            print(e)
+        await self.update_async()
+
+    async def update_progress(self, status: bool):
+        self.is_convert = status
+        self.progress.visible = status
+        await self.update_async()
+
+    async def task_convert(self, files, preset: str, publish_preset: str):
+        """ポッドキャストの音声を変換(タスク)"""
         from zunda_w import __main__
         from zunda_w.arg import Options
+        from zunda_w.edit import edit_from_yml
         files = list(filter(lambda x: x is not None, files))
         conf = OmegaConf.structured(Options(audio_files=files, preset=preset))
+
+        publish_conf = read_preset(publish_preset, os.environ["APP_PUBLISH_PRESET_DIR"])
+        if publish_conf is None:
+            await self.update_progress(False)
+            return
         if self.output_dir.value:
             conf.target_dir = self.output_dir.value
-        __main__._convert(conf)
+
+        if publish_conf.get("mode", "s2t2s") == "s2t2s":
+            self.output_file = __main__._convert(conf)
+        elif publish_conf.get("mode") == "editor":
+            # TODO 修正
+            # TODO anthropicのAPI
+            # TODO 文字おこしの時系列順でショーノートを作る
+
+            conf.preset = preset
+            conf = OmegaConf.to_container(conf, structured_config_mode=SCMode.INSTANTIATE)
+            _audio = edit_from_yml(files, publish_conf)
+            _audio = effects.normalize(_audio)
+            output_audio = conf.tool_output("mix.wav")
+            _audio.export(output_audio, format="wav")
+            conf.audio_files = [output_audio]
+            print(output_audio)
+            from zunda_w.whisper_json import (
+                transcribe_with_config,
+                whisper_context,
+            )
+            from zunda_w.llm import shownote
+            with whisper_context():
+                audio_hash = file_hash(output_audio)
+                stt_file = list(
+                    transcribe_with_config(
+                        conf.audio_files,
+                        conf.whisper_profile,
+                        root_dir=os.path.join(conf.data_dir, audio_hash),
+                        meta_data="",
+                    ))
+                print(stt_file)
+                compose = sort_srt_files(stt_file, word_filter=WordFilter(conf.word_filter))
+                output_srt = conf.tool_output("stt.srt")
+                compose.to_srt(output_srt)
+                if "description" in self.podcast_meta:
+                    output_show_note = conf.tool_output("show_note.md")
+                    if not os.path.exists(output_show_note):
+                        print("Create Show Note")
+
+                        transcript = "\n".join(map(lambda x: x.content, read_srt(output_srt)))
+                        show_note, title = shownote.create_show_note(transcript, self.podcast_meta["description"])
+                        Path(output_show_note).write_text(show_note)
+                    else:
+                        print(f"Show Note Exists: {output_show_note}")
+                print(file_uri(str(conf.tool_output)))
+            self.output_file = output_audio
 
         self.progress.visible = False
         self.is_convert = False
+        self.publish_button.visible = True
         await self.update_async()
 
     async def convert(self, e):
+        """ポッドキャストの音声を変換"""
         files = list(map(lambda x: x.path, self.audio_files.controls))
-        prest = self.preset_select.value
-        print(files, prest)
+        preset = self.preset_select.value
+        publish_preset = self.publish_select.value
+        print(files, preset, publish_preset)
         self.is_convert = True
         self.progress.visible = True
         await self.update_async()
-        asyncio.create_task(self.task_convert(files, prest))
+        asyncio.create_task(self.task_convert(files, preset, publish_preset))
 
     async def add(self, e):
         audio_file_picker = AudioFile(self.task_status_change, self.delete)
