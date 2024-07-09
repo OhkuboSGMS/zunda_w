268ac5fad8a3688fd1498dedb3479549b2eb9261
OhkuboSGMS <ginger.os777146th@gmail.com>
2024-07-09 21:44:53 +0900

    WordFilterに正規表現を設定できるよう修正

--- zunda_w/words.py
+++ zunda_w/words.py
@@ -1,19 +1,24 @@
 import os.path
+import re
 from pathlib import Path
-from typing import Set
+from typing import Set, Tuple
 
 
 class WordFilter:
     def __init__(self, config_path: str):
-        self.data: Set[str] = self.read(config_path)
+        self.data, self.patterns = self.read(config_path)
 
     @classmethod
-    def read(cls, config_path: str) -> Set[str]:
+    def read(cls, config_path: str) -> Tuple[Set[str], Tuple]:
+        word, pattern = [], []
         if not os.path.exists(config_path):
-            return set()
-        return set(
-            map(str.strip, Path(config_path).read_text(encoding="UTF-8").splitlines())
-        )
+            return set(), ()
+        for line in Path(config_path).read_text(encoding="UTF-8").splitlines():
+            if line.startswith("r"):
+                pattern.append(re.compile(line.replace("r", "").strip()))
+            else:
+                word.append(line.strip())
+        return set(word), tuple(pattern)
 
     def is_exclude(self, word: str) -> bool:
-        return word not in self.data
+        return not (word in self.data or any(p.search(word) for p in self.patterns))
