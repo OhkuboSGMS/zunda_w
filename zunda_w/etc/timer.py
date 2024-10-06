0a1b575e37f2fc20ceecf72e123fdc5203a93d34
OhkuboSGMS <ginger.os777146th@gmail.com>
2024-10-06 21:06:06 +0900

    add timer class

---
+++ zunda_w/etc/timer.py
@@ -0,0 +1,16 @@
+import time
+
+
+class Timer:
+    def __init__(self):
+        self.start = None
+        self.end = None
+        self.elapsed = None
+
+    def __enter__(self):
+        self.start = time.perf_counter()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.end = time.perf_counter()
+        self.elapsed = self.end - self.start
