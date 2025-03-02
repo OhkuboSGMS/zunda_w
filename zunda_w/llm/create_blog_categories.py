d6b287d429168abe945f04d0ad72a73eb4bd56a4
OhkuboSGMS <ginger.os777146th@gmail.com>
2025-03-02 19:36:46 +0900

    use langchain-openai

--- zunda_w/llm/create_blog_categories.py
+++ zunda_w/llm/create_blog_categories.py
@@ -1,45 +1,21 @@
-import os
 import re
 from typing import List
 
-import openai
-
-MODEL_NAME = "gpt-4o"
+from langchain.chat_models import init_chat_model
+from langchain_core.messages import SystemMessage, HumanMessage
 
 
 def create(srt_text: str) -> List[str]:
     # 正規表現パターン
     pattern = r'\w+'
-    openai.api_key = os.getenv("OPENAI_API_KEY")
-    response = openai.ChatCompletion.create(
-        model=MODEL_NAME,
-        messages=[
-            {
-                "role": "system",
-                "content": [
-                    {
-                        "text": "ポッドキャストの文字起こしをしたテキストを与えます。\n文字起こしをブログ記事として投稿する際のカテゴリを最大5個まで考えてください。必ず5個作成する必要はありません。\n\n\n#出力例\n[ポッドキャスト,アーカイブ,カメラ]",
-                        "type": "text"
-                    }
-                ]
-            },
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": f"## 入力\n{srt_text}\n\n##出力"
-                    }
-                ]
-            }
-        ],
-        temperature=0.1,
-        max_tokens=256,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
-    category_text = response.choices[0].message.content
+    llm = init_chat_model(model="gpt-4o",model_provider="openai",
+                          temperature=0.1, max_tokens=256, top_p=1, frequency_penalty=0, presence_penalty=0)
+    messages = [
+        SystemMessage("ポッドキャストの文字起こしをしたテキストを与えます。\n文字起こしをブログ記事として投稿する際のカテゴリを最大5個まで考えてください。必ず5個作成する必要はありません。\n\n\n#出力例\n[ポッドキャスト,アーカイブ,カメラ]"),
+        HumanMessage(f"## 入力\n{srt_text}\n\n##出力")
+    ]
+    response = llm.invoke(messages)
+    category_text = response.text()
     # 単語ごとに分割
     categories = re.findall(pattern, category_text)
     return categories
